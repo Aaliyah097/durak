df77dbaa75782d909cf17188f316f19e43c517b9
Aaliyah097 <name.boltz@gmail.com>
2023-11-12 20:18:47 +0300

    reset cards

--- cards/routes.py
+++ cards/routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter, HTTPException, status, Cookie, Response, Depends
 from cards.cards import Card, CardsCollection, StateEnum
 from typing import Annotated
 from fastapi import Body
+from fastapi.responses import RedirectResponse
 
 
 cards_routers = APIRouter(
@@ -48,6 +49,21 @@ async def list_cards(response: Response,
     return cards
 
 
+@cards_routers.get("/stats", status_code=status.HTTP_200_OK)
+async def get_stats(stats: Annotated[dict, Depends(get_stats)]) -> dict:
+    return stats
+
+
+@cards_routers.get("/reset", status_code=status.HTTP_302_FOUND, response_class=RedirectResponse)
+async def reset_cards(user_cards: Annotated[tuple[list[Card], int], Depends(get_user_collection)] = None):
+    for card in user_cards[0]:
+        card.state = "deck"
+
+    users_collections[user_cards[1]] = user_cards[0]
+
+    return '/'
+
+
 @cards_routers.post("/{pk}", status_code=status.HTTP_200_OK)
 async def change_card_state(pk: int,
                             new_state: Annotated[StateEnum, Body()],
@@ -62,8 +78,3 @@ async def change_card_state(pk: int,
     users_collections[user_cards[1]] = user_cards[0]
 
     return card
-
-
-@cards_routers.get("/stats", status_code=status.HTTP_200_OK)
-async def get_stats(stats: Annotated[dict, Depends(get_stats)]) -> dict:
-    return stats
